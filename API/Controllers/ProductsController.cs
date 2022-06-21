a5dd0cf3652c62fb914dfff3c935acad9403a235
Dinindu Gunathilaka <dinindugunathilaka@gmail.com>
2022-06-22 02:13:30 +0530

    Added two end points to get products

--- API/Controllers/ProductsController.cs
+++ API/Controllers/ProductsController.cs
@@ -1,11 +1,27 @@
+using API.Data;
+using API.Entities;
 using Microsoft.AspNetCore.Mvc;
 
 namespace API.Controllers
 {
     [ApiController]
     [Route("api/[controller]")]
-    public class ProductsController
+    public class ProductsController : ControllerBase
     {
-        
+        private readonly StoreContext context;
+
+        public ProductsController(StoreContext context)
+        {
+            this.context = context;
+        }
+
+        [HttpGet]
+        public ActionResult<List<Product>> GetProducts(){
+            var Products=context.Products.ToList();
+            return Ok(Products);
+        }
+
+        [HttpGet("{id}")]
+        public ActionResult<Product> GetProduct(int id) => context.Products.Find(id);
     }
 }
