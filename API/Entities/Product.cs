43d34e0b44e31e511d9493b24637399a7a6c533a
Dinindu Gunathilaka <dinindugunathilaka@gmail.com>
2022-06-21 09:09:47 +0530

    Product entity created

---
+++ API/Entities/Product.cs
@@ -0,0 +1,16 @@
+namespace API.Entities
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = null!;
+        public string Description { get; set; }= string.Empty;
+        public long Price { get; set; }
+        public string? PictureUrl { get; set; }
+        public string Type { get; set; }= default!; 
+        public string Brand { get; set; }= string.Empty;
+
+        public int QuantityInStock { get; set; }
+
+    }
+}
